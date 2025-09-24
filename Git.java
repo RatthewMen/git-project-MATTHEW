efd8f74bdec6a7e6ad4124d087c63c5ac78ab265
Matthew <mren1@hwemail.com>
2025-09-24 12:55:37 -0700

    (GP-2.1): Program Can Initialize Repository
    makeGitDirectoryAndFiles makes the git directory with another directory called objects and 2 files called HEAD and index. This is also the method that initalizes the repository.

--- Git.java
+++ Git.java
@@ -1,5 +1,52 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Git{
-    public public static void main(String[] args) {
+
+    public static void main(String[] args) {
+        makeGitDirectoryAndFiles();
         
+    }
+
+    public static void makeGitDirectoryAndFiles(){
+
+        File directory = new File("git");
+        File objects = new File(directory.getPath(), "objects");
+        File index = new File(directory.getPath(), "index");
+        File HEAD = new File(directory.getPath(), "HEAD");
+
+        if (directory.exists() && objects.exists() && index.exists() && HEAD.exists()){
+            System.out.println("Git Repository Already Exists");
+            return;
+        }
+
+        //makes git directory
+        if (!directory.exists()){
+            directory.mkdir();
+        }
+        //makes object directory inside of git
+        if (!objects.exists()){
+            objects.mkdir();
+        }
+        // makes the index file inside of git
+        try {
+            if (!index.exists()){
+                index.createNewFile();
+            }
+        } catch (Exception e) {
+            System.out.println("Error creating: " + index);
+        }
+        //makes the HEAD file inside of git
+        try {
+            if (!HEAD.exists()){
+                HEAD.createNewFile();
+            }
+        } catch (Exception e) {
+            System.out.println("Error creating: " + HEAD);
+        }
+        System.out.println("Git Repository Created");
+
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # git-project-MATTHEW
+
+makeGitDirectoryAndFiles makes the git directory with another directory called objects and 2 files called HEAD and index. This is also the method that initalizes the repository. 
